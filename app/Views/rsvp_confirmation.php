befec0ca99f523744b790957f355452f07b2ffd7
Dimas <prawira.dimas.yudha@gmail.com>
2025-01-16 02:46:28 +0700

    update rsvp.

--- app/Views/rsvp_confirmation.php
+++ app/Views/rsvp_confirmation.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <author>Dimas Yudha Prawira</author>
+  <meta charset="utf-8">  
   <title>BRI microfinance outlook 2025</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
+  <meta content="BRI Microfinance Outlook 2025" name="keywords">
   <meta content="" name="description">
+  <meta content="Dimas Yudha Prawira" name="author">
 
   <!-- Favicons -->
   <link href="assets/images/favicon.png" rel="icon">

b4139eb11ab197fa5be6b483a567c6e6a994034c
Dimas <prawira.dimas.yudha@gmail.com>
2025-01-16 15:49:52 +0700

    update timetrait

--- system/I18n/TimeTrait.php
+++ system/I18n/TimeTrait.php
@@ -265,13 +265,13 @@ trait TimeTrait
      *
      * @throws Exception
      */
-    public static function createFromTimestamp(int $timestamp, $timezone = null, ?string $locale = null)
+    public static function createFromTimestamp(int|float $timestamp): static
     {
-        $time = new self(gmdate('Y-m-d H:i:s', $timestamp), 'UTC', $locale);
+        // Create the instance from a timestamp
+        $instance = new static('@' . $timestamp);
 
-        $timezone ??= date_default_timezone_get();
-
-        return $time->setTimezone($timezone);
+        // Add any custom logic you need
+        return $instance;
     }
 
     /**

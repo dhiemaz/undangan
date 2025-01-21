d23d9b51cb7273bc90eac57844bc7c1ef729ab6d
Dimas <prawira.dimas.yudha@gmail.com>
2025-01-21 23:37:36 +0700

    update web

--- app/Views/rsvp_confirmation.php
+++ app/Views/rsvp_confirmation.php
@@ -242,7 +242,7 @@
  use function PHPUnit\Framework\isNull;
 
  var_dump($attendee);
- die()
+ die();
 
  if (!isNull($attendee) && !isset($attendee['status'])): ?>
           <form onsubmit="return false;" method="post" action="https://brimicrofinanceoutlook.id/bri-microfinance-2025/invitation/confirm" id="form-submit">

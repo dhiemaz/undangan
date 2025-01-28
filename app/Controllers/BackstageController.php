7ceb167a3af0d24e2d1af57ab16d4bc1935c9458
Dimas <prawira.dimas.yudha@gmail.com>
2025-01-28 17:47:35 +0700

    update web

--- app/Controllers/BackstageController.php
+++ app/Controllers/BackstageController.php
@@ -274,7 +274,7 @@ class BackStageController extends BaseController
     $updatedInvitations = $attendeeModel
         ->select('title, fullname, position, institution, status, type')
         ->where('status IS NOT NULL', null, false)
-        ->orderBy('updated_at', 'DESC')
+        ->orderBy('updated_at', 'ASC')
         ->paginate($perPage, 'default', $page);
 
     // Calculate total records for pagination
